57ce2162e03c579dabdbe3a2cc8cedef2c2b3c22
Wick <vic_the_gunners@hotmail.com>
2024-07-10 13:28:28 +0700

    page user delete

--- src/app/admin/users/DeleteBtn.tsx
+++ src/app/admin/users/DeleteBtn.tsx
@@ -1,26 +1,37 @@
-"use client"
-import React from 'react'
+"use client";
+import React from 'react';
 
-function DeleteBtn({ id }) {
+interface DeleteBtnProps {
+    id: string; // ระบุชนิดข้อมูลของ id เป็น string
+}
 
+function DeleteBtn({ id }: DeleteBtnProps) {
     const handleDelete = async () => {
-        const confirmed = confirm('Are you sure you want to delete this user?')
+        const confirmed = window.confirm('Are you sure you want to delete this user?');
 
         if (confirmed) {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/totalusers?id=${id}`, {
-                method: 'DELETE',
-            })
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/totalusers?id=${id}`, {
+                    method: 'DELETE',
+                });
 
-            if (res.ok) {
-                window.location.reload()
+                if (res.ok) {
+                    window.location.reload();
+                } else {
+                    throw new Error('Failed to delete user');
+                }
+            } catch (error) {
+                console.error(error);
+                // Handle error
             }
         }
-    }
+    };
+
     return (
         <a onClick={handleDelete} className='bg-red-500 text-white border px-2 py-1 rounded-md text-md my-2 ml-2 cursor-pointer'>
             ลบ
         </a>
-    )
+    );
 }
 
-export default DeleteBtn
+export default DeleteBtn;
