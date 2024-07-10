83f49da8a55f818614ecd29c892551a2d0e982e5
Wick <vic_the_gunners@hotmail.com>
2024-07-10 14:14:24 +0700

    container

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,38 +4,46 @@ import Logo from '../../../public/next.svg'
 import Image from 'next/image'
 import { signOut } from 'next-auth/react'
 
+interface NavbarProps {
+  session: any;  // หรือคุณสามารถใช้ประเภทที่เหมาะสมแทน 'any'
+}
 
-
-
-function Navbar({ session }) {
-    return (
-        <nav className='shadow-xl'>
-            <div className='container mx-auto'>
-                <div className='flex justify-between items-center p-4'>
-                    <div>
-                        <Link href="/">
-                            <Image src={Logo} alt="logo" width={100} height={100} />
-                        </Link>
-                    </div>
-                    <ul className='flex'>
-                        {!session ? (
-                            <>
-                                <li className='mx-3'><Link href="/login">เข้าสู่ระบบ</Link></li>
-                                <li className='mx-3'><Link href="/register">ลงทะเบียน</Link></li>
-                            </>
-                        ) : (
-
-                            <li className='mx-3'>
-                                <Link className='bg-gray-500 text-white border px-2 py-1 rounded-md text-md my-2' href="/welcome">โปรไฟล์</Link>
-                                <a className='bg-red-500 text-white border px-2 py-1 rounded-md text-md my-2 ml-2 cursor-pointer' onClick={() => signOut()}>ออกจากระบบ</a>
-                            </li>
-                        )}
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+const Navbar: React.FC<NavbarProps> = ({ session }) => {
+  return (
+    <nav className='shadow-xl'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center p-4'>
+          <div>
+            <Link href="/">
+              <Image src={Logo} alt="logo" width={100} height={100} />
+            </Link>
+          </div>
+          <ul className='flex'>
+            {!session ? (
+              <>
+                <li className='mx-3'>
+                  <Link href="/login">เข้าสู่ระบบ</Link>
+                </li>
+                <li className='mx-3'>
+                  <Link href="/register">ลงทะเบียน</Link>
+                </li>
+              </>
+            ) : (
+              <li className='mx-3'>
+                <Link className='bg-gray-500 text-white border px-2 py-1 rounded-md text-md my-2' href="/welcome">โปรไฟล์</Link>
+                <a 
+                  className='bg-red-500 text-white border px-2 py-1 rounded-md text-md my-2 ml-2 cursor-pointer' 
+                  onClick={() => signOut()}
+                >
+                  ออกจากระบบ
+                </a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
